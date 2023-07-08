a62d76c2f1654bbe85304a2e0a8e98d0a801b4be
Hussainsab <echmg1@gmail.com>
2023-07-09 02:48:27 +0530

    card-qtify-takehome

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import CardBody from "../Cardbody/CardBody";
 import CardFooter from "../CardFooter/CardFooter";
 import style from "./Card.module.css";
-import CardImage from "../CardImage/CardImage";
 
 const Card = () => {
   return (
