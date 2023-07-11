6baf09cbb83f3cb8a84454077bf55a254b483dea
Hussainsab <echmg1@gmail.com>
2023-07-11 14:05:24 +0530

    grid-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import CardSection from "./components/CardSection/CardSection";
 import { useEffect, useState } from "react";
 import axios from "axios";
 function App() {
-  const [topAlbums, setTopAlbums] = useState();
+  const [topAlbums, setTopAlbums] = useState([]);
 
   async function getAlbum() {
-    let response = await axios.get(
-      "https://qtify-backend-labs.crio.do/albums/top"
-    );
-    setTopAlbums(response.data);
-    console.log(response.data);
+    try {
+      let response = await axios.get(
+        "https://qtify-backend-labs.crio.do/albums/top"
+      );
+      setTopAlbums(response.data);
+    } catch (error) {
+      throw new Error("fetch faild");
+    }
   }
 
   useEffect(() => {
